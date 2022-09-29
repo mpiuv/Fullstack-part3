ba0fcc29eb98d284290c61cf528dda2f6931c2d9
T. L <43107946+mpiuv@users.noreply.github.com>
2022-09-29 12:16:12 +0300

    3.8

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 var morgan = require('morgan')
-app.use(morgan('tiny'))
+morgan.token('post',(req,res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 let persons = [
     {
